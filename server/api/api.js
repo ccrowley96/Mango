28cab4f3a35423e35aaec5685d3b30ce1599cc93
Cory <ccrowley96@gmail.com>
2019-04-15 22:35:23 -0400

    npm config stuff

--- server/api/api.js
+++ server/api/api.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const router = express.Router();
 
-const { mongoose } = require("./../db/mongoose");
-const { Computer } = require("./../db/models/rooms");
+router.get("/", (req, res, next) => {
+	res.send("Welcome to the Mango API : )");
+});
 
+// An api endpoint that returns a short list of items
+router.get('/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+
+    setTimeout(() => {res.json(list);}, 2000);
+    
+});
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const path = require('path');
 const api = require("./api/api.js");
-const app = express()
+const bodyParser = require('body-parser');
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+const express = require('express')
+const path = require('path');
+
+let port = process.env.PORT || 8888;
+
+const app = express();
 
 //Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + "/public"))
+	.use(cors())
+	.use(cookieParser());
+
 app.use("/api", api);
 
-app.get('/', function (req, res) {    
-    res.sendfile('mango/client/index.html', {'root': '../'});
-    //res.sendFile(path.join(__dirname+'/client/index.html'), {'root' : '../'});
-})
+if (process.env.NODE_ENV === 'PROD') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, '/../client/build')));
+	console.log('PATH: ', path.join(__dirname, '/../client/build'))
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+	  res.sendFile(path.join(__dirname, './../frontend/build', 'index.html'));
+	});
+}
 
-app.listen(3005, function () {
+app.listen(port, function () {
     console.log('You got Mango\'d!')
 })
