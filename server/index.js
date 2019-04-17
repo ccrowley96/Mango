68b0108602986d1b44d74d372ee2de86ca7a050c
Cory <ccrowley96@gmail.com>
2019-04-17 17:59:23 -0400

    converted mongo uri to string before connecting to database

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI.toString(), {useNewUrlParser: true})
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
 
